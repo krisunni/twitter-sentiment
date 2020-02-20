dd301329d0c26ef9c1b07dd16260573d6db8c3c3
Kris Unnikannan <krish.unnikannan@gmail.com>
2020-02-20 10:56:19 -0500

    Fixing some stuff for CI build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     assetsDir: 'static',
-    outputDir: 'C:\\Users\\Kris.Beta\\AnypointStudio\\studio-workspace\\twitter-plot\\backend\\twitter-plot\\dist'
+    outputDir: '/var/www/html/'
 }
